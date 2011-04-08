9cc6b0cdeeeca4f3a575be91086615bd5ed97610
Luke Amdor <luke.amdor@t8webware.com>
2011-04-08 08:06:38 -0500

    not added the license header if already there

--- src/main/scala/LicenseHeaders.scala
+++ src/main/scala/LicenseHeaders.scala
@@ -4,27 +4,38 @@ import java.io.File
 trait LicenseHeaders extends BasicScalaProject {
   lazy val formatLicenseHeaders = task {
     mainSources.get.foldLeft(None: Option[String]) { (result, path) =>
-      val Right(fileContents) = FileUtilities.readBytes(path.asFile, log)
-      val destination = new File(path.projectRelativePath + ".withHeader")
       result orElse {
-        FileUtilities.append(destination, commentedLicenseText, log)
-      } orElse {
-        FileUtilities.append(destination, lineSeparator, log)
-      } orElse {
-        FileUtilities.append(destination, fileContents, log)
-      } orElse {
-        FileUtilities.copyFile(destination, path.asFile, log)
+        val Right(fileContents) = FileUtilities.readString(path.asFile, log)
+        val destination = new File(path.projectRelativePath + ".withHeader")
+        
+        if (alreadyHasHeader(fileContents)) None else {
+          FileUtilities.append(destination,
+                               commentedLicenseTextLines.mkString(lineSeparator),
+                               log)
+        } orElse {
+          FileUtilities.append(destination, lineSeparator, log)
+        } orElse {
+          FileUtilities.append(destination, fileContents, log)
+        } orElse {
+          FileUtilities.copyFile(destination, path.asFile, log)
+        }
       }
     }
   }
   
   def licenseText: String
 
-  def commentedLicenseText: String = {
+
+  lazy val commentedLicenseTextLines: List[String] = {
     val commentedLines = licenseText.lines.map { line => " * " + line }.toList
-    ("/*" :: commentedLines ::: " */" :: Nil) mkString (lineSeparator)
+    ("/*" :: commentedLines ::: " */" :: Nil)
   }
 
   private val lineSeparator = System.getProperty("line.separator")
+  
+  private def alreadyHasHeader(fileContents: String): Boolean =
+    fileContents.lines.toList.zip(commentedLicenseTextLines) forall {
+      case (fileLine, commentLine) => fileLine == commentLine
+    }
   
 }
---
+++ src/sbt-test/projects/add-license-already-there/src/main/scala/MyClass.scala
@@ -0,0 +1,13 @@
+/*
+ * 
+ * Copyright (c) Initech Corp. 2011
+ * 
+ * Licensed under your mom.
+ * Not really.
+ * Just don't sue us.
+ */
+package something
+
+class MyClass {
+  def doSomething = "hello"
+}
