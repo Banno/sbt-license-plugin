940893b6fe8c3f8184ce00f7eef353cd3b061619
Luke Amdor <luke.amdor@t8webware.com>
2011-04-10 19:52:37 -0500

    run sbt-test's on test

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -4,7 +4,6 @@ class Project(info: ProjectInfo) extends PluginProject(info) with test.ScalaScri
   override def scriptedSbt = "0.7.5"
   override def scriptedBufferLog = false
 
-  override def testAction = testNoScripted
-  lazy val default = scripted dependsOn(publishLocal) describedAs("Publishes locally and tests against example projects")
+  override def testAction = scripted
 }
 
